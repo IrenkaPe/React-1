708a96a925aeae017b11a3bdb744d76265c864c7
Klaudia Perszke <klaudia.perszke@gmail.com>
2025-10-08 13:21:32 +0200

    Initial project commit

---
+++ src/components/SearchForm/SearchForm.js
@@ -0,0 +1,20 @@
+import styles from "./SearchForm.module.scss"
+import TextInput from '../TextInput/TextInput';
+import Button from '../Button/Button';
+
+const SearchForm = () => {
+    return (
+        <form className={styles.searchForm}>
+            <TextInput placeholder="Search..."/>
+
+            <Button>
+                <span className="fa fa-search" />
+            </Button>
+            <TextInput placeholder="Add.."/>
+            
+            <Button>Create</Button>
+        </form>
+    );
+  };
+
+  export default SearchForm;
